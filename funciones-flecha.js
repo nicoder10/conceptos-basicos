53844fb114ba75005e3719ee17cf852ef8ae2d85
Nicolas Martino <martinonicolas10@gmail.com>
2021-07-12 11:31:56 -0300

    Initial commit with basic excercises

---
+++ funciones-flecha.js
@@ -0,0 +1,10 @@
+var sumar = (x, y) => x + y;
+var restar = (x, y) => x - y;
+var multiplicar = (x, y) => x * y;
+var dividir = (x, y) => x / y;
+
+
+console.log(sumar(1, 1));
+console.log(restar(2, 1));
+console.log(multiplicar(2, 1));
+console.log(dividir(2, 1));
